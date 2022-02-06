261b8510f0f2b29d64a442a82a241b26660ded3b
Pratyush Dixit <83740557+pratyushdixit27@users.noreply.github.com>
2022-02-06 11:29:07 +0530

    Add files via upload

---
+++ tut6.java
@@ -0,0 +1,13 @@
+
+    public class tut66 {
+    public static void main(String[]args){
+    String s="Deepak";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+
+        int a = 10;  int b=20;
+        String s1=String.valueOf(a);
+        String s2=String.valueOf(b);
+        System.out.println(s1+s2);
+    }
+}
